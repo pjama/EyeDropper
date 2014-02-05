e1dfcfebfd39c3ea3f189b2dec25c095f0559a93
Philip Jama <phil.jama@gmail.com>
2014-02-04 17:43:50 -0800

    separated Picker color sample, improved styling for lists

--- src/com/jamasan/eyedropper/HUD.java
+++ src/com/jamasan/eyedropper/HUD.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class HUD {
 	private FullscreenActivity mActivity;
 	private PickerFragment mPickerFragment;
+	
+	private FrameLayout mColorFrame;
+	private ImageView mColorSwatch;
+	private TextView mColorTitle;
+	private TextView mColorSubitle;
+	
 	private ListView mListColors;
 	private CustomAdapter mCustomAdapter;
 	private ArrayList<CustomListItem> mListRowItems;
@@ -31,6 +41,12 @@ public class HUD {
 		mListRowItems = new ArrayList<CustomListItem>();
 		mListColors = (ListView)activity.findViewById(R.id.list_colors);
 		
+		mColorFrame = (FrameLayout)activity.findViewById(R.id.color_picker_frame);
+		mColorSwatch = (ImageView)activity.findViewById(R.id.picker_color_swatch);
+		mColorTitle = (TextView)activity.findViewById(R.id.picker_color_title);
+		mColorSubitle = (TextView)activity.findViewById(R.id.picker_color_description);
+		
+		mColorFrame.setOnClickListener(onClickListener);
 		mCustomAdapter = new CustomAdapter(mActivity, R.layout.row_custom, mListRowItems);
 		mListColors.setAdapter(mCustomAdapter);
 		mListColors.setOnItemClickListener(onItemClickListener);
@@ -57,9 +73,9 @@ public class HUD {
 		mColors.clear();
 		mListRowItems.clear();
 		
-		mBaseColor.setName(mBaseColor.getRGB());
-		mBaseColor.setDescription("0x"+mBaseColor.getHex());
-		mListRowItems.add(new CustomListItem(mBaseColor));
+		mColorSwatch.setBackgroundColor(mBaseColor.getARGB());
+		mColorTitle.setText("0x"+mBaseColor.getHex());
+		mColorSubitle.setText(mBaseColor.getRGB());
 		
 		ColorPoint colorWebSafe = mBaseColor.getWebSafeColor();
 		mListRowItems.add(new CustomListItem(colorWebSafe));
@@ -73,19 +89,34 @@ public class HUD {
 		((CustomAdapter)mListColors.getAdapter()).notifyDataSetChanged();
 	}
 	
+	private void showColorDetail(ColorPoint color) {
+		ColorSample colorSample = new ColorSample(color.toBundle());
+		colorSample.setSource(mPickerFragment.getColorSource().getSourceType());
+		DetailFragment fragment = new DetailFragment();
+		fragment.setArguments(color.toBundle());
+		mActivity.setActiveFragment(fragment);
+	}
+	
+	OnClickListener onClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.color_picker_frame:
+				showColorDetail(mBaseColor);
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	
 	OnItemClickListener onItemClickListener = new OnItemClickListener() {
-
 		@Override
 		public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
 			CustomAdapter adapter = (CustomAdapter)parent.getAdapter();
 			CustomListItem item = adapter.getItem(pos);
 			ColorSample color = new ColorSample(item.getColor().toBundle());
-			color.setSource(mPickerFragment.getColorSource().getSourceType());
-			
-			DetailFragment fragment = new DetailFragment();
-			fragment.setArguments(color.toBundle());
-			
-			mActivity.setActiveFragment(fragment);
+			showColorDetail(color);
 		}
 	};
 }
